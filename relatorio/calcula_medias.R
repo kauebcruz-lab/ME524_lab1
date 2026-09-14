f43c59d7c82dcbfcdcb244d96017d71177cb3c34
acvaleweb <antocyrineu@gmail.com>
2026-09-14 02:00:53 -0300

    Update calcula_medias.R
    typo

--- relatorio/calcula_medias.R
+++ relatorio/calcula_medias.R
@@ -3,7 +3,7 @@ calcula_medias <- function(dataframe)
   
     group_by(dataframe, time) %>%
     summarise(
-      tetha = mean(gols_marcados),
+      theta = mean(gols_marcados),
       phi   = mean(gols_sofridos),
       
       .groups = "drop"
