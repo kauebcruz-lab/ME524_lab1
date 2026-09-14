bddf1799ab3441618d3d3fe88dda3062d6c4c4cf
acvaleweb <antocyrineu@gmail.com>
2026-09-14 18:41:04 -0300

    Update tratar_dados.R
    deixar tratamento de input igual ao das outras funções

--- relatorio/tratar_dados.R
+++ relatorio/tratar_dados.R
@@ -1,21 +1,28 @@
-tratar_dados <- function(dataframe) 
+tratar_dados <- function(
+  dataframe,
+  coluna_rodada = rodada,
+  coluna_time_mandante = time_mandante,
+  coluna_gols_mandante = gols_mandante,
+  coluna_time_visitante = time_visitante,
+  coluna_gols_visitante = gols_visitante
+) 
 {
   # filtrando NAs
-  dataframe <- filter(dataframe, !is.na(gols_mandante), !is.na(gols_visitante))
+  dataframe <- filter(dataframe, !is.na({{ coluna_gols_mandante }}), !is.na({{ coluna_gols_visitante }}))
   
   # listando jogos em casa
   jogos_casa <- select(dataframe, 
-                       rodada,
-                       time = time_mandante, 
-                       gols_marcados = gols_mandante, 
-                       gols_sofridos = gols_visitante)
+                       rodada = {{ coluna_rodada }},
+                       time = {{ coluna_time_mandante }}, 
+                       gols_marcados = {{ coluna_gols_mandante }}, 
+                       gols_sofridos = {{ coluna_gols_visitante }})
   
   # listando jogos fora de casa
   jogos_fora <- select(dataframe,
-                       rodada,
-                       time = time_visitante,
-                       gols_marcados = gols_visitante,
-                       gols_sofridos = gols_mandante)
+                       rodada = {{ coluna_rodada }},
+                       time = {{ coluna_time_visitante }},
+                       gols_marcados = {{ coluna_gols_visitante }},
+                       gols_sofridos = {{ coluna_gols_mandante }})
   
   # unificando em uma única tabela e ordenando
   bind_rows(jogos_casa,jogos_fora) %>%
