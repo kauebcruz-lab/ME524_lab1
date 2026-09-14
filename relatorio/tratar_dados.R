b030e2f82cdb5a9fec41d7b3f79e7fb5117322e3
acvaleweb <antocyrineu@gmail.com>
2026-09-14 02:53:36 -0300

    Update tratar_dados.R

--- relatorio/tratar_dados.R
+++ relatorio/tratar_dados.R
@@ -5,19 +5,20 @@ tratar_dados <- function(dataframe)
   
   # listando jogos em casa
   jogos_casa <- select(dataframe, 
-                       time = time_mandante, 
                        rodada,
+                       time = time_mandante, 
                        gols_marcados = gols_mandante, 
                        gols_sofridos = gols_visitante)
   
   # listando jogos fora de casa
   jogos_fora <- select(dataframe,
-                       time = time_visitante,
                        rodada,
+                       time = time_visitante,
                        gols_marcados = gols_visitante,
                        gols_sofridos = gols_mandante)
   
-  # unificando em uma única tabela
-  bind_rows(jogos_casa,jogos_fora)
+  # unificando em uma única tabela e ordenando
+  bind_rows(jogos_casa,jogos_fora) %>%
+  arrange(rodada, time)
   
 }
